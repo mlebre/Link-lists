5e1c386c2159b400b742b81391bc9d3f48f44adf
Lebre Marianne <mlebre@bs406-s16-12.insa-lyon.fr>
2014-12-01 08:23:14 +0100

    File with all the librairies and file's included

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <cstdio>
 #include <cstdlib>
+#include "image.h"
 #include "node.h"
 
-int main ()
+
+//Work about the link lists
+int main(int argc, char const *argv[])
 {
-  node test;
-  printf("Hello World!\n");
-  return 0;
+	image testo;
+	node one;
+	printf("%d, %d \n",one.Get_essai(), testo.Get_height());
+
+
+	return 0;
 }
